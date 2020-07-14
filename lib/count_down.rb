abbaee63d2a9af9dd0c2da28022d9c4e48a12058
Maysoon Tabali <mtab03@me.com>
2020-07-14 19:41:33 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,8 @@
 # Write your code here
 exit_number = 0
 count = 10
-while count > 1 do 
-
-
-
-
+while count < 1 do 
+  break if count == exit_number 
+  puts "Happy New Year"
 count = count - 1 
+end 
